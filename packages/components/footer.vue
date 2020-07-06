9dc5fe99d409a328c997c8371b91be66fb2fd00a
BaoRongLong <2305613385@qq.com>
2020-07-06 17:49:39 +0800

    增加公共组件

---
+++ packages/components/footer.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="Tect">
+        <h3>公共模块-底部</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Tect',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
